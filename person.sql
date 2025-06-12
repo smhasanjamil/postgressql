a0abc3d408b078873b9e6be70b7e72e507771ad1
smhasanjamil <smhasanjamil23@gmail.com>
2025-06-12 06:11:07 +0600

    Add employee table creation and data insertion

--- person.sql
+++ person.sql
@@ -7,7 +7,6 @@ DROP DATABASE persondb;
 
 -- Crud Operation
 
--- Creating Table
 -- Creating a new table
 CREATE TABLE person (
     id INT,
@@ -35,3 +34,45 @@ SELECT * FROM person;
 
 -- Reading specific column data from table
 SELECT id, name from person;
+
+-- Update data from a table
+UPDATE person SET city = 'Khulna' WHERE id = 103;
+
+-- Deleting data from a table
+DELETE FROM person WHERE id = 105;
+
+-- Most widely used data type are INT DOUBLE FLOAT DECIMAL VARCHAR DATE BOOLEAN
+
+-- Create table with constrain
+CREATE TABLE employees (
+    emp_id SERIAL PRIMARY KEY,
+    fname VARCHAR(50) NOT NULL,
+    lname VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    dept VARCHAR(50),
+    salary DECIMAL(10, 2) DEFAULT 30000.00,
+    hire_date DATE NOT NULL DEFAULT CURRENT_DATE
+);
+
+-- Insert Data to employee table
+INSERT INTO
+    employees (
+        emp_id,
+        fname,
+        lname,
+        email,
+        dept,
+        salary,
+        hire_date
+    )
+VALUES (
+        104,
+        'Sajibur',
+        'Rahman',
+        'sajib2@gmail.com',
+        'IT',
+        35000,
+        '2025-06-12'
+    );
+
+    
